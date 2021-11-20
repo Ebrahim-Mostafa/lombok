84ae45a32d4900fadaae0bf9b40813bc1cb1bcc7
Ebahim Mostafa <57369953+Ebrahim-Mostafa@users.noreply.github.com>
2021-11-20 17:53:05 +0200

    lombok project

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,6 @@
+
+:
+
+testng.xml,5/2/522a7a4e21649af06297e31a9e33f13bb4fd3022
+V
+&src/test/java/tests/contactUsTest.java,5/3/535abc4e45f1e4264b76cdafbd7c2ab5f540fd91
---
+++ src/test/java/tests/joinUsSectionTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import BasePackage.BaseTest;
+import Pages.joinUsSection;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class joinUsSectionTest extends BaseTest {
+
+    private static joinUsSection joinUsSection;
+
+    @BeforeClass
+    public void initialization() {
+        joinUsSection = new joinUsSection();
+    }
+
+
+    @Test(priority = 3)
+    public void joinUsSection() {
+        joinUsSection.joinUsSectionPage();
+        joinUsSection.filterByLocation();
+        joinUsSection.applyPosition();
+        joinUsSection.verifyRequiredErrorMessages();
+        joinUsSection.verifyInvalidErrorMessages();
+    }
+}
